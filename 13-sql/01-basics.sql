066ed03bf28ca8974e7b100b92b2f7b496a29abd
Sebastian <ksgomez421@misena.edu.co>
2022-08-19 11:43:26 -0500

    se agrergan nuevos archivos php y se inicia sql

---
+++ 13-sql/01-basics.sql
@@ -0,0 +1,6 @@
+-- To acces Data Bases (Mysql) in a web browser
+-- https://localhost/phpmyadmin;
+
+-- To acces local Data Base (Mysql) in command line (Windows):
+-- open control panel 
+mysql -u root -p 
