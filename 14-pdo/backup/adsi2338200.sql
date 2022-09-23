d0e1661b8686384d53db052fc06b641d21ddaf03
Sebastian <ksgomez421@misena.edu.co>
2022-09-23 08:34:13 -0500

    small changes

--- 14-pdo/backup/adsi2338200.sql
+++ 14-pdo/backup/adsi2338200.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 09-09-2022 a las 15:38:48
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.3.15
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-09-2022 a las 15:19:34
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -65,7 +64,7 @@ CREATE TABLE `pokemons` (
 
 INSERT INTO `pokemons` (`id`, `name`, `type`, `strength`, `stamina`, `speed`, `accuracy`, `image`, `trainer_id`) VALUES
 (1, 'pikachu', 'Electric', 90, 80, 96, 79, 'public/images/pokeball.png', 1),
-(2, 'charmander', 'Fire', 95, 78, 80, 82, 'public/images/pokeball.png', 1),
+(2, 'Guru', 'Fire', 50, 23, 44, 0, 'public/images/1663343058.jpg', 1),
 (3, 'bulbasaour', 'Grass', 80, 88, 70, 75, 'public/images/pokeball.png', 1),
 (4, 'squirtle', 'Water', 70, 90, 75, 90, 'public/images/pokeball.png', 2),
 (5, 'Snorlax', 'Normal', 180, 320, 50, 180, 'public/images/pokeball.png', 1),
@@ -83,7 +82,11 @@ INSERT INTO `pokemons` (`id`, `name`, `type`, `strength`, `stamina`, `speed`, `a
 (17, 'Magnemite', 'Electric', 750, 50, 40, 128, 'public/images/pokeball.png', 4),
 (18, 'Pidgey', 'Normal', 818, 80, 95, 90, 'public/images/pokeball.png', 1),
 (19, 'Gastly', 'Ghost', 750, 60, 60, 82, 'public/images/pokeball.png', 4),
-(20, 'Rattata', 'Normal', 810, 60, 65, 22, 'public/images/pokeball.png', 1);
+(20, 'Rattata', 'Normal', 810, 60, 65, 22, 'public/images/pokeball.png', 1),
+(21, 'charmando', 'Dragon', 55, 55, 55, 55, 'public/images/pokeball.png', 1),
+(24, 'Eevee', 'Normal', 22, 22, 22, 22, 'public/images/1663334535.jpg', 1),
+(25, 'Giratina', 'Dragon', 100, 100, 100, 100, 'public/images/1663336071.jpg', 1),
+(29, 'esteban', 'Normal', 20, 20, 25, 25, 'public/images/1663936792.jpg', 2);
 
 -- --------------------------------------------------------
 
@@ -95,6 +98,9 @@ CREATE TABLE `trainers` (
   `id` int(11) NOT NULL,
   `name` varchar(32) NOT NULL,
   `level` int(11) NOT NULL DEFAULT 1,
+  `email` varchar(32) NOT NULL,
+  `photo` varchar(64) NOT NULL DEFAULT 'public/images/trainer.png',
+  `password` varchar(64) NOT NULL DEFAULT '827ccb0eea8a706c4c34a16891f84e7b',
   `gym_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -102,12 +108,12 @@ CREATE TABLE `trainers` (
 -- Volcado de datos para la tabla `trainers`
 --
 
-INSERT INTO `trainers` (`id`, `name`, `level`, `gym_id`) VALUES
-(1, 'Ash Ketchum', 1, 1),
-(2, 'Brock', 5, 2),
-(3, 'Misty', 4, 2),
-(4, 'Serena', 4, 2),
-(5, 'Oak', 9, 1);
+INSERT INTO `trainers` (`id`, `name`, `level`, `email`, `photo`, `password`, `gym_id`) VALUES
+(1, 'Ash Ketchum', 1, 'ash@gmail.com', 'public/images/trainer.png', '827ccb0eea8a706c4c34a16891f84e7b', 1),
+(2, 'Brock', 5, 'brock@gmail.com', 'public/images/trainer.png', '827ccb0eea8a706c4c34a16891f84e7b', 2),
+(3, 'Misty', 4, 'misty@gmail.com', 'public/images/trainer.png', '827ccb0eea8a706c4c34a16891f84e7b', 2),
+(4, 'Serena', 4, 'serena@gmail.com', 'public/images/trainer.png', '827ccb0eea8a706c4c34a16891f84e7b', 2),
+(5, 'Oak', 9, 'oak@gmail.com', 'public/images/trainer.png', '827ccb0eea8a706c4c34a16891f84e7b', 1);
 
 -- --------------------------------------------------------
 
@@ -128,7 +134,7 @@ CREATE TABLE `view_pokemons` (
 --
 DROP TABLE IF EXISTS `view_pokemons`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_pokemons`  AS  select `trainers`.`name` AS `name_trainer`,`gyms`.`name` AS `name_gym`,count(`pokemons`.`id`) AS `num_pokemons` from ((`trainers` left join `gyms` on(`trainers`.`gym_id` = `gyms`.`id`)) left join `pokemons` on(`pokemons`.`trainer_id` = `trainers`.`id`)) group by `trainers`.`id` ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_pokemons`  AS SELECT `trainers`.`name` AS `name_trainer`, `gyms`.`name` AS `name_gym`, count(`pokemons`.`id`) AS `num_pokemons` FROM ((`trainers` left join `gyms` on(`trainers`.`gym_id` = `gyms`.`id`)) left join `pokemons` on(`pokemons`.`trainer_id` = `trainers`.`id`)) GROUP BY `trainers`.`id``id`  ;
 
 --
 -- Índices para tablas volcadas
@@ -154,6 +160,7 @@ ALTER TABLE `pokemons`
 --
 ALTER TABLE `trainers`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
   ADD KEY `gym_id` (`gym_id`);
 
 --
@@ -170,7 +177,7 @@ ALTER TABLE `gyms`
 -- AUTO_INCREMENT de la tabla `pokemons`
 --
 ALTER TABLE `pokemons`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT de la tabla `trainers`
